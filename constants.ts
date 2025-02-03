593f1f697ae4534de812054b1eba56feeafa6fed
thechibuzor1 <chibuzorm26@gmail.com>
2025-02-03 02:07:40 +0100

    proper typescript typing

--- constants.ts
+++ constants.ts
@@ -43,68 +43,260 @@ export type FloatingSvg = {
  * Static data
  */
 
-export const auctions: Auction[] = [
-  {
-    title: "Tristique diam a, enim, eros tellus. Viverra etiam",
-    value: 2.55,
-    time: "2:41",
-    likes: 54,
-    bids: 14,
-    cover: "/images/auction/Auctionimg1.jpeg",
-    profiles: [
-      "/images/profiles/profile1.jpeg",
-      "/images/profiles/profile2.jpeg",
-      "/images/profiles/profile3.jpeg",
-      "/images/profiles/profile4.jpeg",
-    ],
-  },
-  {
-    title: "Vulputate felis purus viverra morbi facilisi eget",
-    value: 3.19,
-    time: "2:41",
-    likes: 120,
-    bids: 35,
-    cover: "/images/auction/Auctionimg2.jpeg",
-    profiles: [
-      "/images/profiles/profile1.jpeg",
-      "/images/profiles/profile2.jpeg",
-      "/images/profiles/profile3.jpeg",
-      "/images/profiles/profile4.jpeg",
-    ],
-  },
-];
 
-export const nfts: NFT[] = [
-  {
-    title: "Vulputate velit viverra volutpat volutpat tristique",
-    value: 1.25,
-    time: "22:59",
-    bids: 101,
-    cover: "/images/nfts/blkimg1.png",
-  },
-  {
-    title: "Egestas blandit sit egestas non sed. Purus semper",
-    value: 1.25,
-    time: "22:59",
-    bids: 101,
-    cover: "/images/nfts/blkimg2.png",
-  },
-];
+export const auctions:Auction[] = [
+    {
+      title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+      value: 2.55,
+      time: "2:41",
+      likes: 54,
+      bids: 14,
+      cover: "/images/auction/Auctionimg1.jpeg",
+      profiles: [
+        "/images/profiles/profile1.jpeg",
+        "/images/profiles/profile2.jpeg",
+        "/images/profiles/profile3.jpeg",
+        "/images/profiles/profile4.jpeg",
+      ],
+    },
+    {
+      title: "Vulputate felis purus viverra morbi facilisi eget",
+      value: 3.19,
+      time: "2:41",
+      likes: 120,
+      bids: 35,
+      cover: "/images/auction/Auctionimg2.jpeg",
+      profiles: [
+        "/images/profiles/profile1.jpeg",
+        "/images/profiles/profile2.jpeg",
+        "/images/profiles/profile3.jpeg",
+        "/images/profiles/profile4.jpeg",
+      ],
+    },
+    {
+      title: "Dui accumsan leo vestibulum ornare eu",
+      value: 1.11,
+      time: "22:59",
+      likes: 570,
+      bids: 101,
+      cover: "/images/auction/Auctionimg3.jpeg",
+      profiles: [
+        "/images/profiles/profile7.jpeg",
+        "/images/profiles/profile5.jpeg",
+        "/images/profiles/profile8.jpeg",
+        "/images/profiles/profile11.jpeg",
+      ],
+    },
+    {
+      title: "Senectus adipiscing nascetur accumsan etiam",
+      value: 1.63,
+      time: "37:01",
+      likes: 98,
+      bids: 12,
+      cover: "/images/auction/Auctionimg4.jpeg",
+      profiles: [
+        "/images/profiles/profile1.jpeg",
+        "/images/profiles/profile2.jpeg",
+        "/images/profiles/profile3.jpeg",
+        "/images/profiles/profile4.jpeg",
+      ],
+    },
+    {
+      title: "Mattis at diam lorem nisl nam sed sociis",
+      value: 2.55,
+      time: "12:15",
+      likes: 540,
+      bids: 19,
+      cover: "/images/auction/Auctionimg5.jpeg",
+      profiles: [
+        "/images/profiles/profile5.jpeg",
+        "/images/profiles/profile6.jpeg",
+        "/images/profiles/profile7.jpeg",
+        "/images/profiles/profile8.jpeg",
+      ],
+    },
+    {
+      title: "Senectus adipiscing nascetur accumsan etiam",
+      value: 1.63,
+      time: "37:01",
+      likes: 98,
+      bids: 12,
+      cover: "/images/auction/Auctionimg4.jpeg",
+      profiles: [
+        "/images/profiles/profile9.jpeg",
+        "/images/profiles/profile10.jpeg",
+        "/images/profiles/profile11.jpeg",
+      ],
+    },
+    {
+      title: "Mattis at diam lorem nisl nam sed sociis",
+      value: 2.55,
+      time: "12:15",
+      likes: 540,
+      bids: 19,
+      cover: "/images/auction/Auctionimg5.jpeg",
+      profiles: [
+        "/images/profiles/profile12.jpeg",
+        "/images/profiles/profile13.jpeg",
+        "/images/profiles/profile14.jpeg",
+        "/images/profiles/profile16.jpeg",
+      ],
+    },
+    {
+      title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+      value: 2.55,
+      time: "2:41",
+      likes: 54,
+      bids: 14,
+      cover: "/images/auction/Auctionimg1.jpeg",
+      profiles: [
+        "/images/profiles/profile10.jpeg",
+        "/images/profiles/profile12.jpeg",
+        "/images/profiles/profile13.jpeg",
+        "/images/profiles/profile4.jpeg",
+      ],
+    },
+    {
+      title: "Vulputate felis purus viverra morbi facilisi eget",
+      value: 3.19,
+      time: "2:41",
+      likes: 120,
+      bids: 35,
+      cover: "/images/auction/Auctionimg2.jpeg",
+      profiles: [
+        "/images/profiles/profile1.jpeg",
+        "/images/profiles/profile2.jpeg",
+        "/images/profiles/profile3.jpeg",
+        "/images/profiles/profile4.jpeg",
+      ],
+    },
+    {
+      title: "Dui accumsan leo vestibulum ornare eu",
+      value: 1.11,
+      time: "22:59",
+      likes: 570,
+      bids: 101,
+      cover: "/images/auction/Auctionimg3.jpeg",
+      profiles: [
+        "/images/profiles/profile10.jpeg",
+        "/images/profiles/profile2.jpeg",
+        "/images/profiles/profile13.jpeg",
+        "/images/profiles/profile4.jpeg",
+      ],
+    },
+    {
+      title: "Senectus adipiscing nascetur accumsan etiam",
+      value: 1.63,
+      time: "37:01",
+      likes: 98,
+      bids: 12,
+      cover: "/images/auction/Auctionimg4.jpeg",
+      profiles: [
+        "/images/profiles/profile1.jpeg",
+        "/images/profiles/profile7.jpeg",
+        "/images/profiles/profile13.jpeg",
+        "/images/profiles/profile4.jpeg",
+      ],
+    },
+    {
+      title: "Mattis at diam lorem nisl nam sed sociis",
+      value: 2.55,
+      time: "12:15",
+      likes: 540,
+      bids: 19,
+      cover: "/images/auction/Auctionimg5.jpeg",
+      profiles: [
+        "/images/profiles/profile1.jpeg",
+        "/images/profiles/profile2.jpeg",
+        "/images/profiles/profile3.jpeg",
+        "/images/profiles/profile4.jpeg",
+      ],
+    },
+    {
+      title: "Senectus adipiscing nascetur accumsan etiam",
+      value: 1.63,
+      time: "37:01",
+      likes: 98,
+      bids: 12,
+      cover: "/images/auction/Auctionimg4.jpeg",
+      profiles: [
+        "/images/profiles/profile3.jpeg",
+        "/images/profiles/profile12.jpeg",
+        "/images/profiles/profile11.jpeg",
+        "/images/profiles/profile4.jpeg",
+      ],
+    },
+    {
+      title: "Mattis at diam lorem nisl nam sed sociis",
+      value: 2.55,
+      time: "12:15",
+      likes: 540,
+      bids: 19,
+      cover: "/images/auction/Auctionimg5.jpeg",
+      profiles: [
+        "/images/profiles/profile3.jpeg",
+        "/images/profiles/profile2.jpeg",
+        "/images/profiles/profile8.jpeg",
+        "/images/profiles/profile6.jpeg",
+      ],
+    },
+  ];
 
-export const stat_data: Stat[] = [
-  {
-    svg: "profile.svg",
-    number: "300k",
-    stat: "Users Active",
-  },
-  {
-    svg: "Artworks.svg",
-    number: "52,5k",
-    stat: "Artworks",
-  },
-];
+export const nfts:NFT[] = [
+    {
+      title: "Vulputate velit viverra volutpat volutpat tristique",
+      value: 1.25,
+      time: "22:59",
+      bids: 101,
+      cover: "/images/nfts/blkimg1.png",
+    },
+    {
+      title: "Egestas blandit sit egestas non sed. Purus semper",
+      value: 1.25,
+      time: "22:59",
+      bids: 101,
+      cover: "/images/nfts/blkimg2.png",
+    },
+    {
+      title: "Id ut consequat netus nec vel amet ut porta in augue",
+      value: 1.25,
+      time: "22:59",
+      bids: 101,
+      cover: "/images/nfts/blkimg3.png",
+    },
+    {
+      title: "Sed et nibh felis integer tellus turpin",
+      value: 1.25,
+      time: "22:59",
+      bids: 101,
+      cover: "/images/nfts/blkimg4.png",
+    },
+  ];
 
-export const popular_auctions: Auction[] = [
+export const stat_data:Stat[] = [
+    {
+      svg: "profile.svg",
+      number: "300k",
+      stat: "Users Active",
+    },
+    {
+      svg: "Artworks.svg",
+      number: "52,5k",
+      stat: "Artworks",
+    },
+    {
+      svg: "artists.svg",
+      number: "17,5k",
+      stat: "Artists",
+    },
+    {
+      svg: "eth.svg",
+      number: "35.58",
+      stat: "ETH Spent",
+    },
+  ];
+
+export const popular_auctions:Auction[] =  [
   {
     title: "Tristique diam a, enim, eros tellus. Viverra etiam",
     value: 2.55,
@@ -113,9 +305,80 @@ export const popular_auctions: Auction[] = [
     bids: 14,
     cover: "/images/popular/p1.jpeg",
   },
+  {
+    title: "Vulputate felis purus viverra morbi facilisi eget",
+    value: 3.19,
+    time: "2:41",
+    likes: 120,
+    bids: 35,
+    cover: "/images/popular/p2.jpeg",
+  },
+  {
+    title: "Dui accumsan leo vestibulum ornare eu",
+    value: 1.11,
+    time: "22:59",
+    likes: 570,
+    bids: 101,
+    cover: "/images/popular/p3.jpeg",
+  },
+  {
+    title: "Senectus adipiscing nascetur accumsan etiam",
+    value: 1.63,
+    time: "37:01",
+    likes: 98,
+    bids: 12,
+    cover: "/images/popular/p4.jpeg",
+  },
+  {
+    title: "Mattis at diam lorem nisl nam sed sociis",
+    value: 2.55,
+    time: "12:15",
+    likes: 540,
+    bids: 19,
+    cover: "/images/popular/p5.jpeg",
+  },
+  {
+    title: "Senectus adipiscing nascetur accumsan etiam",
+    value: 1.63,
+    time: "37:01",
+    likes: 98,
+    bids: 12,
+    cover: "/images/popular/p6.jpeg",
+  },
 ];
-
-export const more_from: Auction[] = [
+export const more_from:Auction[] =  [
+  {
+    title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+    value: 2.55,
+    time: "2:41",
+    likes: 54,
+    bids: 14,
+    cover: "/images/morefrom/mf1.jpeg",
+  },
+  {
+    title: "Vulputate felis purus viverra morbi facilisi eget",
+    value: 3.19,
+    time: "2:41",
+    likes: 120,
+    bids: 35,
+    cover: "/images/morefrom/mf2.jpeg",
+  },
+  {
+    title: "Dui accumsan leo vestibulum ornare eu",
+    value: 1.11,
+    time: "22:59",
+    likes: 570,
+    bids: 101,
+    cover: "/images/morefrom/mf3.jpeg",
+  },
+  {
+    title: "Senectus adipiscing nascetur accumsan etiam",
+    value: 1.63,
+    time: "37:01",
+    likes: 98,
+    bids: 12,
+    cover: "/images/morefrom/mf4.jpeg",
+  },
   {
     title: "Mattis at diam lorem nisl nam sed sociis",
     value: 2.55,
@@ -124,13 +387,37 @@ export const more_from: Auction[] = [
     bids: 19,
     cover: "/images/morefrom/mf5.jpeg",
   },
+  {
+    title: "Senectus adipiscing nascetur accumsan etiam",
+    value: 1.63,
+    time: "37:01",
+    likes: 98,
+    bids: 12,
+    cover: "/images/morefrom/mf6.jpeg",
+  },
 ];
 
-export const scrollingNfts: ScrollingNFT[] = [
+
+export const scrollingNfts:ScrollingNFT[] = [
   { fileName: "images/scrollingnfts/b1.jpeg", name: "2.55 ETH" },
   { fileName: "images/scrollingnfts/b2.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b3.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b5.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b6.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b7.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b8.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b9.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b10.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b11.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b12.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b13.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b14.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b15.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b16.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b17.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b18.jpeg", name: "2.55 ETH" },
+  { fileName: "images/scrollingnfts/b19.jpeg", name: "2.55 ETH" },
 ];
-
 
 export const floatingSvgs:FloatingSvg[] = [
   { fileName:  "images/floatingnfts/f4.jpeg", name: "2.55 ETH", top: "15%", left: "75%" },
